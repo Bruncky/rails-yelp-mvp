bc7521d58b52491bd84c024c50788e33d6dfcc4c
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 15:26:08 +0100

    Built Restaurants Controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,31 @@
+# frozen_string_literal: true
+
+# Restaurants Controller
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end

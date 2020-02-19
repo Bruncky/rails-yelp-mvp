4d403b611e477f89ba1a4c66e2dc2ea4aa30996b
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 12:26:15 +0100

    Restaurant Migration done

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_19_112234) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "number"
+    t.string "category"
+    t.integer "review", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end

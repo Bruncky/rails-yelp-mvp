b1cb644ea5f96d3ae353767fa406026e7e0423dc
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 14:18:50 +0100

    Added validations to Review Model

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,10 @@
 # Review Model
 class Review < ApplicationRecord
   belongs_to :restaurant
+
+  validates :restaurant_id, presence: true
+  validates :content, presence: true
+
+  validates :rating, presence: true, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true }
 end

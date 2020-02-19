c1e1ef95b537a25e6de2e0cdc713daf0f61dfa56
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 15:26:50 +0100

    Fixed routes with nesting

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: %i[index show new create]
-
-  post 'restaurants/:restaurant_id/reviews', to: 'reviews#index'
-  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new'
+  resources :restaurants, only: %i[index show new create] do
+    resources :reviews, only: %i[new create]
+  end
 end

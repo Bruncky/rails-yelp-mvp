4ce6cbfc7db29978e75b91b57e7b1e85628dcffc
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 15:26:23 +0100

    Built Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
+# Reviews Controller
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review.create(review_params, restaurant_id: @restaurant.id)
+    redirect_to restaurant_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end

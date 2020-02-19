67902ae86e225fce934e4422d678f80adb309a90
Bruncky <brunobrunckhorst@gmail.com>
2020-02-19 18:54:16 +0100

    Fixed bug where review wouldn't create properly

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,18 +2,25 @@
 
 # Reviews Controller
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: %i[new create]
+
   def new
     @review = Review.new
   end
 
   def create
-    @review.create(review_params, restaurant_id: @restaurant.id)
-    redirect_to restaurant_path
+    @review = Review.create(review_params)
+    redirect_to @restaurant
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    # params.require(:review).permit(:content, :rating)
+    { content: params[:content], rating: params[:rating], restaurant_id: @restaurant.id }
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
